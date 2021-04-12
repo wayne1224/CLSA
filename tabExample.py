c830ffc95469f3e01775bf94e442f8d6f677afba
Kenneth Ding <58462256+Kenneth-Ding@users.noreply.github.com>
2021-04-12 14:49:18 +0800

    更改命名空間

--- tabExample.py
+++ tabExample.py
@@ -1,10 +1,10 @@
 import sys
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
 from Mytable import Mytable
 from myform import Myform
-class TabDemo(QTabWidget):
+class TabDemo(QtWidgets.QTabWidget):
     def __init__(self,parent=None):
         super(TabDemo, self).__init__(parent)
 
@@ -13,7 +13,7 @@ class TabDemo(QTabWidget):
         #创建3个选项卡小控件窗口
         self.tab1=Myform()
         self.tab2=Mytable()
-        self.tab3=QWidget()
+        self.tab3=QtWidgets.QWidget()
 
 
 
@@ -26,22 +26,22 @@ class TabDemo(QTabWidget):
         self.setStyleSheet( "QTabBar::tab { height: 40px; width: 250px; }")
 
    
-        self.tab3UI()
+    #     self.tab3UI()
 
-    def tab3UI(self):
-        #水平布局
-        layout=QHBoxLayout()
+    # def tab3UI(self):
+    #     #水平布局
+    #     layout=QHBoxLayout()
 
-        #添加控件到布局中
-        layout.addWidget(QLabel('科目'))
-        layout.addWidget(QCheckBox('物理'))
-        layout.addWidget(QCheckBox('高数'))
+    #     #添加控件到布局中
+    #     layout.addWidget(QLabel('科目'))
+    #     layout.addWidget(QCheckBox('物理'))
+    #     layout.addWidget(QCheckBox('高数'))
 
-        #设置小标题与布局方式
-        self.setTabText(2,'教育程度')
-        self.tab3.setLayout(layout)
+    #     #设置小标题与布局方式
+    #     self.setTabText(2,'教育程度')
+    #     self.tab3.setLayout(layout)
 if __name__ == '__main__':
-    app=QApplication(sys.argv)
+    app=QtWidgets.QApplication(sys.argv)
     demo=TabDemo()
     demo.show()
     sys.exit(app.exec_())
