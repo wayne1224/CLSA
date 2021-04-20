fa06abfbfb6ce7f7048d0638b7f5644c828c5f39
00757001 <00757001@email.ntou.edu.tw>
2021-04-21 00:07:38 +0800

    feat: 新增分析頁面

--- MainWindow.py
+++ MainWindow.py
@@ -3,6 +3,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 from Tab2 import Tab2
 from Tab1 import Myform
+from Tab3 import Analysis
 class MainTabWidget(QtWidgets.QTabWidget):
     def __init__(self,parent=None):
         super(MainTabWidget, self).__init__(parent)
@@ -12,12 +13,12 @@ class MainTabWidget(QtWidgets.QTabWidget):
         #創建3個tab
         self.tab1=Myform()
         self.tab2=Tab2()
-        self.tab3=QtWidgets.QWidget()
+        self.tab3=Analysis()
 
         #將tab加入MainWindow中
         self.addTab(self.tab1, "收錄表")
         self.addTab(self.tab2, "轉錄表")
-        self.addTab(self.tab3, "Tab 3")
+        self.addTab(self.tab3, "彙整表")
 
         #設定tab的css
         self.setStyleSheet( "QTabBar::tab { height: 40px; width: 250px; }")
