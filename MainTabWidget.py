e357e9e4fbf2ad9fa50f06c77cba3387d9bd7f7a
00757001 <00757001@email.ntou.edu.tw>
2021-05-04 03:40:07 +0800

    新增Mainwindow Widget

---
+++ MainTabWidget.py
@@ -0,0 +1,76 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from Tab0 import SearchTab
+from Tab2 import Tab2
+from Tab1 import Myform
+from Tab3 import AnalysisTab
+
+class MainTabWidget(QtWidgets.QTabWidget):
+    def __init__(self,parent=None):
+        super(MainTabWidget, self).__init__(parent)
+        self.setWindowTitle("CLSA")
+
+        #創建4個tab
+        self.tab0 = SearchTab()
+        self.tab1 = Myform()
+        self.tab2 = Tab2()
+        self.tab3 = AnalysisTab()
+
+        #模糊特效
+        self.blur_effect = QtWidgets.QGraphicsBlurEffect()
+
+        #將tab加入MainWindow中
+        self.addTab(self.tab0, "查詢頁面")
+        self.addTab(self.tab1, "收錄表")
+        self.addTab(self.tab2, "轉錄表")
+        self.addTab(self.tab3, "彙整表")
+
+        #設定tab的css
+        self.setStyleSheet( "QTabBar::tab { height: 40px; width: 250px; }")
+
+        #設定Tab Signal
+        self.currentChanged.connect(self.checkTab2Changed)
+
+        self.tab1.procStart.connect(self.tab2.setCaseRecord)
+        self.tab2.procUtterNum.connect(self.tab1.getUtterNum)
+        self.tab2.procChildUtter.connect(self.tab3.getChildUtterance)
+        #self.tab2.procKey.connect(self.tab1.~~~)
+        self.tab2.procKey.connect(self.tab3.getKey)
+        self.tab0.procDoc.connect(self.tab3.getKey)
+        self.tab0.procDoc.connect(self.tab1.getDoc)
+        self.tab0.procDoc.connect(self.tab2.getDoc) 
+
+
+    def closeEvent(self, event):
+        if self.tab1.saveExamination() or self.tab2.isEdit():
+            warnText = '<p style="font-size:13pt; color: #3778bf;">要儲存對下列頁面的變更嗎?</p>\n'
+            if self.tab1.saveExamination():
+                warnText += '收錄表\n'
+            if self.tab2.isEdit():
+                warnText += '轉錄表'
+
+            close = QtWidgets.QMessageBox.warning(self,
+                            "CLSA",
+                            warnText,
+                            QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No | QtWidgets.QMessageBox.Cancel)
+            if close == QtWidgets.QMessageBox.Yes and self.tab1.save():
+                event.accept()
+            elif close == QtWidgets.QMessageBox.No:
+                event.accept()
+            else:
+                event.ignore()
+    
+
+
+    def checkTab2Changed(self):
+        if self.currentIndex() == 3 and self.tab2.isEdit():
+            informBox = QtWidgets.QMessageBox.warning(self, '警告','轉錄表已變動 請重新產生彙整表', QtWidgets.QMessageBox.Ok)
+
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    demo = MainTabWidget()
+    demo.show()
+    sys.exit(app.exec_())
--- MainWindow.py
+++ MainWindow.py
@@ -1,74 +1,47 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from Tab0 import SearchTab
-from Tab2 import Tab2
-from Tab1 import Myform
-from Tab3 import AnalysisTab
-class MainTabWidget(QtWidgets.QTabWidget):
-    def __init__(self,parent=None):
-        super(MainTabWidget, self).__init__(parent)
+from MainTabWidget import MainTabWidget
+# Subclass QMainWindow to customise your application's main window
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
         self.resize(1393, 870)
         self.setWindowTitle("CLSA")
+        self.mainTab = MainTabWidget()
+        self.setCentralWidget(self.mainTab)
 
-        #創建4個tab
-        self.tab0 = SearchTab()
-        self.tab1 = Myform()
-        self.tab2 = Tab2()
-        self.tab3 = AnalysisTab()
+        #模糊特效
+        self.blur_effect = QtWidgets.QGraphicsBlurEffect()
+        #self.setGraphicsEffect(self.blur_effect)
 
+        #signal
+        self.mainTab.tab0.procMain.connect(self.getAction)
+        self.mainTab.tab2.procMain.connect(self.getAction)
+        self.mainTab.tab3.procMain.connect(self.getAction)
 
-        #將tab加入MainWindow中
-        self.addTab(self.tab0, "查詢頁面")
-        self.addTab(self.tab1, "收錄表")
-        self.addTab(self.tab2, "轉錄表")
-        self.addTab(self.tab3, "彙整表")
+        # self.label = QtWidgets.QLabel() 
+        # self.label.setObjectName("loading")
+        # self.label.show()
+        # self.movie = QtGui.QMovie('loading.gif')
+        # self.label.setMovie(self.movie) 
+        # self.startAnimation() 
 
-        #設定tab的css
-        self.setStyleSheet( "QTabBar::tab { height: 40px; width: 250px; }")
+    def startAnimation(self): 
+        self.movie.start() 
 
-        #設定Tab Signal
-        self.currentChanged.connect(self.checkTab2Changed)
-
-        self.tab1.procStart.connect(self.tab2.setCaseRecord)
-        self.tab2.procUtterNum.connect(self.tab1.getUtterNum)
-        self.tab2.procChildUtter.connect(self.tab3.getChildUtterance)
-        #self.tab2.procKey.connect(self.tab1.~~~)
-        self.tab2.procKey.connect(self.tab3.getKey)
-        self.tab0.procDoc.connect(self.tab3.getKey)
-        self.tab0.procDoc.connect(self.tab1.getDoc)
-        self.tab0.procDoc.connect(self.tab2.getDoc)
-
-
-    def closeEvent(self, event):
-        if self.tab1.saveExamination() or self.tab2.isEdit():
-            warnText = '<p style="font-size:13pt; color: #3778bf;">要儲存對下列頁面的變更嗎?</p>\n'
-            if self.tab1.saveExamination():
-                warnText += '收錄表\n'
-            if self.tab2.isEdit():
-                warnText += '轉錄表'
-
-            close = QtWidgets.QMessageBox.warning(self,
-                            "CLSA",
-                            warnText,
-                            QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No | QtWidgets.QMessageBox.Cancel)
-            if close == QtWidgets.QMessageBox.Yes and self.tab1.save():
-                event.accept()
-            elif close == QtWidgets.QMessageBox.No:
-                event.accept()
-            else:
-                event.ignore()
-    
-
-
-    def checkTab2Changed(self):
-        if self.currentIndex() == 3 and self.tab2.isEdit():
-            informBox = QtWidgets.QMessageBox.warning(self, '警告','轉錄表已變動 請重新產生彙整表', QtWidgets.QMessageBox.Ok)
-
-
-
+    @QtCore.pyqtSlot(int)
+    def getAction(self, key):
+        if key == 1:
+            self.mainTab.setCurrentIndex(1)
+        elif key == 2:
+            pass
+            #self.setGraphicsEffect(self.blur_effect)
+        elif key == 3:
+            pass
+            #self.setGraphicsEffect(0)
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    demo = MainTabWidget()
-    demo.show()
-    sys.exit(app.exec_())
+    window = MainWindow()
+    window.show()
+    app.exec_()
