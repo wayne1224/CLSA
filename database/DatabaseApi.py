782d62d374912edde7438c0de9c91225467bda8c
wayne1224 <waynewayne1224@gmail.com>
2021-11-13 17:05:51 +0800

    Update DatabaseApi.py

--- database/DatabaseApi.py
+++ database/DatabaseApi.py
@@ -485,9 +485,7 @@ def findChildren(caseID , name):
                 }
             }
         ])
-
-        result = list(result)
-                           
+                          
         return result
     except Exception as e:
         print(e)
